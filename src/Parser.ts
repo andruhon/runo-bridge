0efbd16cd3e18413f6adda2152585446eb8119a1
Andrew Kondratev <andruhon@gmail.com>
2016-02-23 23:02:02 +1300

    I want my static typings! Add TypeScript

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs';
 
 export class Parser {
-  constructor(source, type) {
+  constructor(source: NodeJS.ReadableStream) {
     console.log(1);
   }
 
   parse() {
     return new Promise(function(resolve,reject){
+      fs.readdir(".");
       setTimeout(resolve,500);
     });
   }
