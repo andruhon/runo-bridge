557b2da77a578a04fe5f69dbd0821d5955c67c0d
Andrew Kondratev <andruhon@gmail.com>
2016-02-24 23:33:49 +1300

    add basic cli functionality

--- cli.js
+++ cli.js
@@ -1,49 +1,29 @@
 var parser = require("./dist/Parser.js");
 var generator = require("./dist/Generator.js");
 var fs = require("fs");
+var path = require("path");
+var program = require('commander');
 
 // var p = new parser.Parser();
 // p.parse().then(function(){
 //   console.log(2);
 // });
 
-var input = {
-  "module_name": "embed.",
-  "functions": [
-    {
-      "name": "int_in_int_out",
-      "inputs": [
-        {
-          "name": "input",
-          "type": "c_int"
-        }
-      ],
-      "output": "c_int"
-    },
-    {
-      "name": "is_greather_than_42",
-      "inputs": [
-        {
-          "name": "input",
-          "type": "c_int"
-        }
-      ],
-      "output": "bool"
-    },
-    {
-      "name": "get_42",
-      "inputs": [
-        {
-          "name": "input",
-          "type": "bool"
-        }
-      ],
-      "output": "c_int"
-    }
-  ]
-};
+var defaultTemplate =path.join(__dirname,"templates","addon.cc");
 
-var template = fs.readFileSync("templates/addon.cc", 'utf8');
+program
+  .version('0.0.1');
 
-var g = new generator.Generator(input, template);
-console.log(g.generate());
+
+program
+  .command('generate <input>')
+  .description('generate C++ addon from Rust or JSON input')
+  .action(function(input) {
+    console.log("generating from "+input);
+    var template = fs.readFileSync(defaultTemplate, 'utf8');
+    var inputVal = JSON.parse(fs.readFileSync(input, 'utf8'));
+    var g = new generator.Generator(inputVal, template);
+    console.log(g.generate());
+  });
+
+program.parse(process.argv);
