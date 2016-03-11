5fce13dc02c20b8d94f0f16d59c98086892d6de4
Andrew Kondratev <andruhon@gmail.com>
2016-03-11 15:32:49 +1300

    improve integration tests

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,28 +1,41 @@
 os = require('os');
-addon = require('../resources/build/Debug/addon');
+addon = require('../resources/build/Release/addon');
 var assert = require('assert');
 
 describe('Integration', function() {
-  it('should run functions from embed.rs', function () {
-    assert.equal(addon.int_in_int_out(2),4, "int_in_int_out 2");
+  it('should run int_in_int_out 2', function () {
+    assert.equal(addon.int_in_int_out(2),4);
+  });
 
-    assert.equal(addon.is_greather_than_42(50), true, "is_greather_than_42 50");
+  it('should run is_greather_than_42 50', function () {
+    assert.equal(addon.is_greather_than_42(50), true);
+  });
 
-    assert.equal(addon.is_greather_than_42(5), false, "is_greather_than_42 5");
+  it('should run is_greather_than_42 5', function () {
+    assert.equal(addon.is_greather_than_42(5), false);
+  });
 
-    assert.equal(addon.get_42(true), 42, "get_42 true");
+  it('should run get_42 true', function () {
+    assert.equal(addon.get_42(true), 42);
+  });
 
-    assert.equal(addon.get_42(false), 0, "get_42 false");
+  it('should run get_42 false', function () {
+    assert.equal(addon.get_42(false), 0);
+  });
 
+  it('should run rs_experimental_string', function () {
     assert.equal(
       addon.rs_experimental_string("Looooong string from JS dfsdfsdf sd123456789 JSEOM"),
-      "Looooong string from JS dfsdfsdf sd123456789 JSEOM+ append from Rust",
-      "rs_experimental_string"
+      "Looooong string from JS dfsdfsdf sd123456789 JSEOM+ append from Rust"
     );
+  });
 
+  it('should run double_multiply_plus2 3.14, 2', function () {
     assert.equal(addon.double_multiply_plus2(3.14,2), 3.14*2+2, "double_multiply_plus2");
+  });
 
-    assert.equal(Math.round(addon.float_multiply_plus2(3.14,2)), Math.round(3.14*2+2), "float_multiply_plus2 p1");
-    assert.notEqual(addon.float_multiply_plus2(3.14,2), 3.14*2+2, "float_multiply_plus2 p2");
+  it('should run float_multiply_plus2 3.14, 2', function () {
+    assert.equal(Math.round(addon.float_multiply_plus2(3.14,2)), Math.round(3.14*2+2), "approx equal");
+    assert.notEqual(addon.float_multiply_plus2(3.14,2), 3.14*2+2, "not exact equal with double calculation");
   });
 });
